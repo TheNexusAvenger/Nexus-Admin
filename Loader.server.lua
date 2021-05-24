79d20c014569c8d6e0f5423352f245ab9d053c26
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-05-24 16:06:44 -0400

    Increment version.

--- Loader.server.lua
+++ Loader.server.lua
@@ -3,7 +3,7 @@ TheNexusAvenger
 
 Loads Nexus Admin.
 
-Loader version: V.2.1.3
+Loader version: V.2.1.4
 GitHub: https://github.com/thenexusavenger/nexus-admin
 
 For the change log, view the GitHub Releases (except V.1.0.0, V.1.0.1, V.1.1.0, and V.1.2.0).
