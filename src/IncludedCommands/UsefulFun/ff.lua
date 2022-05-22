7ff82711b9f0e856a875b3cdee5d99ab92909df4
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-05-22 00:22:50 -0400

    Add optional parameter for invisible forcefields.

--- src/IncludedCommands/UsefulFun/ff.lua
+++ src/IncludedCommands/UsefulFun/ff.lua
@@ -21,20 +21,30 @@ function Command:__new()
             Name = "Players",
             Description = "Players to give force fields.",
         },
+        {
+            Type = "boolean",
+            Name = "Invisible",
+            Optional = true,
+            Description = "Whether to make the forcefield invisible. Defaults to false.",
+        },
     }
 end
 
 --[[
 Runs the command.
 --]]
-function Command:Run(CommandContext,Players)
+function Command:Run(CommandContext, Players, Invisible)
     self.super:Run(CommandContext)
-    
+
     --Give the forcefields.
     for _,Player in pairs(Players) do
         local Character = Player.Character
         if Character then
-            Instance.new("ForceField").Parent = Character
+            local ForceField = Instance.new("ForceField")
+            if Invisible == true then
+                ForceField.Visible = false
+            end
+            ForceField.Parent = Character
         end
     end
 end
