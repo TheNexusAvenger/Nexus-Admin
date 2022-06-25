faa872ee1a04a8f0fdad4be91eb4437a0ce5f035
Chrisrankingservices <103617014+Chrisrankingservices@users.noreply.github.com>
2022-06-24 22:57:29 -0400

    Update NexusAdminClientLoader.client.lua
    e

--- src/Resources/NexusAdminClientLoader.client.lua
+++ src/Resources/NexusAdminClientLoader.client.lua
@@ -5,6 +5,6 @@ Loads Nexus Admin on the client.
 --]]
 
 local NexusAdminClient = require(game:GetService("ReplicatedStorage"):WaitForChild("NexusAdminClient"))
-NexusAdminClient.Cmdr:SetActivationKeys({Enum.KeyCode.Apostrophe})
+NexusAdminClient.Cmdr:SetActivationKeys({Enum.KeyCode.Backslash})
 NexusAdminClient.Registry:LoadServerCommands()
 NexusAdminClient:LoadIncludedCommands()
