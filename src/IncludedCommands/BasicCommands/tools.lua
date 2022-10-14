05dc9919015fd670ab79f327777e5459a0e1fd2b
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-10-14 18:44:35 -0400

    Check for authorization for getting tools.

--- src/IncludedCommands/BasicCommands/tools.lua
+++ src/IncludedCommands/BasicCommands/tools.lua
@@ -21,31 +21,35 @@ Creates the command.
 --]]
 function Command:__new()
     self:InitializeSuper("tools","BasicCommands","Opens up a window containing the list of tools usable by :give.")
-    
+
     --Create the remote function.
     local GetToolsInContainers = Instance.new("RemoteFunction")
     GetToolsInContainers.Name = "GetToolsInContainers"
     GetToolsInContainers.Parent = self.API.EventContainer
 
     function GetToolsInContainers.OnServerInvoke(Player)
-        --Create the list.
-        local ToolsList = {}
-        for _,Container in pairs(self.Containers) do
-            table.insert(ToolsList,{Text=Container.Name,Font="SourceSansBold"})
-            local ToolsInContainer = ToolFilter("all",{Container})
-            if #ToolsInContainer ~= 0 then
-                for _,Tool in pairs(ToolsInContainer) do
-                    table.insert(ToolsList,Tool.Name)
+        if self.API.Authorization:IsPlayerAuthorized(Player,self.AdminLevel) then
+            --Create the list.
+            local ToolsList = {}
+            for _,Container in pairs(self.Containers) do
+                table.insert(ToolsList,{Text=Container.Name,Font="SourceSansBold"})
+                local ToolsInContainer = ToolFilter("all",{Container})
+                if #ToolsInContainer ~= 0 then
+                    for _,Tool in pairs(ToolsInContainer) do
+                        table.insert(ToolsList,Tool.Name)
+                    end
+                else
+                    table.insert(ToolsList,{Text="(None)",Font="SourceSansItalic"})
                 end
-            else
-                table.insert(ToolsList,{Text="(None)",Font="SourceSansItalic"})
+                table.insert(ToolsList,"")
             end
-            table.insert(ToolsList,"")
-        end
 
-        --Return the list.
-        table.remove(ToolsList,#ToolsList)
-        return ToolsList
+            --Return the list.
+            table.remove(ToolsList,#ToolsList)
+            return ToolsList
+        else
+            return {"Unauthorized"}
+        end
     end
 end
 
