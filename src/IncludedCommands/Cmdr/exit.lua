0342166339fe83569a10da32147e6deef6c04ac7
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-01-07 15:55:26 -0500

    Only use exit in Cmdr console.

--- src/IncludedCommands/Cmdr/exit.lua
+++ src/IncludedCommands/Cmdr/exit.lua
@@ -14,6 +14,14 @@ return {
     Description = "Closes the command line. (Added by Nexus Admin.)",
     AllowAllUsers = true,
     ClientRun = function(CommandContext: Types.CmdrCommandContext)
+        local Util = IncludedCommandUtil.ForContext(CommandContext)
+
+        --Return if it wasn't executed from the command line.
+        if Util:ExecutedFromChat() or Util:ExecutedFromGuiConsole() or Util:ExecutedFromKeybind() then
+            return "This command is only intended to be run from the Cmdr command line."
+        end
+
+        --Toggle the window.
         (IncludedCommandUtil.ForContext(CommandContext):GetApi().Cmdr :: any):Toggle()
         return ""
     end,
