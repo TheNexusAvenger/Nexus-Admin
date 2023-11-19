5b91a314d9b9bc2eb7d2de98f42e673931c3dfa8
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-11-19 00:51:09 -0500

    Increment version.

--- src/Common/Configuration.lua
+++ src/Common/Configuration.lua
@@ -21,7 +21,7 @@ function Configuration.new(ConfigurationTable: {[string]: any}): Types.Configura
     setmetatable(self, Configuration)
 
     --Store the values.
-    self.Version = "Version 2.7.8"
+    self.Version = "Version 2.7.9"
     self.VersionNumberId = 2.7
     self.CmdrVersion = "Version 1.12.0"
     self.RawConfiguration = ConfigurationTable
